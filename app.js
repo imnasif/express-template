9063145dddf1812554c49cb2a790d49a0eb7730f
Nasif Ahmed <nasif.ahmed@infolytx.com>
2017-08-10 15:13:51 +0600

    refactor

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(require('./middlewares/response'));
 app.use(require('./middlewares/logger'));
-app.use(require('./middlewares/error'));
 app.use(require('./controllers'));
 
 app.listen(port, () => console.log('Listening on port ' + port));
